90974eb36fc44c1a603cd842bfb296951be2dd10
Josiah Parry <josiah.parry@gmail.com>
2024-05-12 15:54:52 -0400

    update bindings to support popup, google and github

--- src/bindings/auth.rs
+++ src/bindings/auth.rs
@@ -1,5 +1,5 @@
-use wasm_bindgen::prelude::*;
 use crate::app::FirebaseApp;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -12,7 +12,11 @@ extern "C" {
     pub fn on_auth_state_changed(this: &Auth, callback: &Closure<dyn FnMut(JsValue)>);
 
     #[wasm_bindgen(method, catch, js_name = createUserWithEmailAndPassword)]
-    pub async fn create_user_with_email_and_password(this: &Auth, email: String, password: String) -> Result<JsValue, JsValue>;
+    pub async fn create_user_with_email_and_password(
+        this: &Auth,
+        email: String,
+        password: String,
+    ) -> Result<JsValue, JsValue>;
 
     #[wasm_bindgen(catch, method, js_name = signInWithEmailAndPassword)]
     pub async fn sign_in_with_email_and_password(
@@ -23,4 +27,22 @@ extern "C" {
 
     #[wasm_bindgen(catch, method, js_name = signOut)]
     pub async fn sign_out(this: &Auth) -> Result<JsValue, JsValue>;
+
+    // Google Auth support
+    pub type GoogleAuthProvider;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> GoogleAuthProvider;
+
+    // Github Auth support
+    pub type GithubAuthProvider;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> GithubAuthProvider;
+
+    #[wasm_bindgen(catch, method, js_name = signInWithPopup)]
+    pub async fn sign_in_with_popup(
+        auth: &Auth,
+        provider: GoogleAuthProvider,
+    ) -> Result<JsValue, JsValue>;
 }
